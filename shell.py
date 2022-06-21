8257131098355b93e0905b08201f4c8cbe913d0b
Akon-Kahuak <gorritiignacioe@gmail.com>
2022-06-20 23:03:38 -0300

    Second commit

---
+++ shell.py
@@ -0,0 +1,11 @@
+from sys import stdin
+import basic
+
+while True:
+    text = input('basic >')
+    result, error = basic.run('<stdin>', text)
+
+    if error:
+        print(error.as_string())
+    else:
+        print(result)
