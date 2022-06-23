a10adbee2558e90fcb49d5fe7a87804b77e364f0
Akon-Kahuak <gorritiignacioe@gmail.com>
2022-06-23 00:17:56 -0300

    Added String

--- basic.py
+++ basic.py
@@ -2,7 +2,6 @@
 # IMPORTS
 #######################################
 
-from json import tool
 from strings_with_arrows import *
 import string
 #######################################
@@ -91,6 +90,7 @@ class Position:
 
 TT_INT		= 'INT'
 TT_FLOAT    = 'FLOAT'
+TT_STRING 	= 'STRING'
 TT_IDENTIFIER = 'IDENTIFIER'
 TT_KEYWORD = 'KEYWORD'
 TT_PLUS     = 'PLUS'
@@ -174,6 +174,8 @@ class Lexer:
 				tokens.append(self.make_number())
 			elif self.current_char in LETTERS:
 				tokens.append(self.make_identifier())
+			elif self.current_char == '"':
+				tokens.append(self.make_string())
 			elif self.current_char == '+':
 				tokens.append(Token(TT_PLUS, pos_start=self.pos))
 				self.advance()
@@ -232,6 +234,31 @@ class Lexer:
 			return Token(TT_INT, int(num_str), pos_start, self.pos)
 		else:
 			return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
+
+	def make_string(self):
+		string = ''
+		pos_start = self.pos.copy()
+		escape_character = False
+		self.advance()
+
+		escape_characters = {
+			'n': '\n',
+			't': '\t',
+		}
+
+		while self.current_char != None and (self.current_char != '"' or escape_character):
+			if escape_character:
+				string += escape_characters.get(self.current_char, self.current_char)
+			else:
+				if self.current_char == '\\':
+					escape_character = True
+				else:
+					string += self.current_char
+			self.advance()
+			escape_character = False
+
+		self.advance()
+		return Token(TT_STRING, string, pos_start, self.pos)
 
 	def make_identifier(self):
 		id_str = ''
@@ -303,6 +330,17 @@ class Lexer:
 #######################################
 
 class NumberNode:
+	def __init__(self, tok):
+		self.tok = tok
+
+		self.pos_start = self.tok.pos_start
+		self.pos_end = self.tok.pos_end
+
+	def __repr__(self):
+		return f'{self.tok}'
+
+
+class StringNode:
 	def __init__(self, tok):
 		self.tok = tok
 
@@ -599,6 +637,11 @@ class Parser:
 			res.register_advancement()
 			self.advance()
 			return res.success(NumberNode(tok))
+
+		if tok.type in TT_STRING:
+			res.register_advancement()
+			self.advance()
+			return res.success(StringNode(tok))
 
 		elif tok.type == TT_IDENTIFIER:
 			res.register_advancement()
@@ -1108,6 +1151,37 @@ class Number(Value):
 	def __repr__(self):
 		return str(self.value)
 
+class String(Value):
+	def __init__(self, value):
+		super().__init__()
+		self.value = value
+
+	def added_to(self, other):
+		if isinstance(other, String):
+			return String(self.value + other.value).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
+	
+	def multed_by(self, other):
+		if isinstance(other, Number):
+			return String(self.value * other.value).set_context(self.context), None
+		else:
+			return None, Value.illegal_operation(self, other)
+	
+	def is_true(self):
+		return len(self.value) > 0
+
+	def copy(self):
+		copy = String(self.value)
+		copy.set_pos(self.pos_start, self.pos_end)
+		copy.set_context(self.context)
+		return copy
+	
+	def __repr__(self):
+		return f'"{self.value}"'
+
+	
+
 class Function(Value):
 	def __init__(self, name, body_node, arg_names):
 		super().__init__()
@@ -1205,6 +1279,11 @@ class Interpreter:
 	def visit_NumberNode(self, node, context):
 		return RTResult().success(
 			Number(node.tok.value).set_context(context).set_pos(node.pos_start, node.pos_end)
+		)
+
+	def visit_StringNode(self, node, context):
+		return RTResult().success(
+			String(node.tok.value).set_context(context).set_pos(node.pos_start, node.pos_end)
 		)
 
 	def visit_VarAccessNode(self, node, context):
